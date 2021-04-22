810cb2ca809c96bfba1d417ef5d3af4c21b2662a
davidmorasalazar <davidmsalazar0299@gmail.com>
2021-04-22 13:38:00 -0600

    Update Tarea 1 Macroeconometría.py

--- Tarea 1 Macroeconometría.py
+++ Tarea 1 Macroeconometría.py
@@ -21,7 +21,7 @@ a=pd.read_csv(io.StringIO(s1.decode('utf-8')))
 b = a.rename(columns = {0: 'Time'}, inplace = False)
 pd.options.display.max_rows = 20
 print(a)
-# #1.c
+#1.c
 a.plot(subplots=True, figsize=(10, 10)); plt.legend(loc='best')
 #1.c.alternativa1
 fig, axes = plt.subplots(nrows=2, ncols=2)
@@ -29,21 +29,35 @@ a['Ozone'].plot(ax=axes[0,0]); axes[0,0].set_title('Ozone')
 a['Temp'].plot(ax=axes[0,1]); axes[0,1].set_title('Temp')
 a['Solar.R'].plot(ax=axes[1,0]); axes[1,0].set_title('Solar.R')
 a['Wind'].plot(ax=axes[1,1]); axes[1,1].set_title('Wind')
-# #1.c.alternativa2
-# # y = ["Ozone"]
-# # x = ["Day"]
-# # g = sns.FacetGrid(a, row='Ozone', col='Day')
-# # g.map(sns.lmplot, "age")
-# # plt.show()
-# #1.d
+#1.c.alternativa2
+# y = ["Ozone"]
+# x = ["Day"]
+# g = sns.FacetGrid(a, row='Ozone', col='Day')
+# g.map(sns.lmplot, "age")
+# plt.show()
+#1.d
 
-#2.a
+# #2.a
 urlb="https://vincentarelbundock.github.io/Rdatasets/csv/datasets/AirPassengers.csv"
 s2=requests.get(urlb).content
 b=pd.read_csv(io.StringIO(s2.decode('utf-8')))
 print(b)
 #2.b
-# sns.lineplot(data=b, x="Unnamed: 0", y="value")
+sns.lineplot(data=b, x="Unnamed: 0", y="value")
 #2.c
 b['Natural logarithm of value'] = np.log(b['value'])
 sns.lineplot(data=b, x="Unnamed: 0", y="Natural logarithm of value")
+#3.a
+df = pd.read_csv(r'C:\Users\MiBebe\Desktop\Economía Universidad de Costa Rica\Macro Econometría\Tarea 1')
+print(df)
+#3.b
+
+#3.c
+
+#3.d
+
+#3.e
+
+#3.f
+
+#4.a
